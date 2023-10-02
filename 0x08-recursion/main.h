6a002dc07fb40d884f217e3a0c3420549e2a52d4
root <root@DESKTOP-HPNLPVB>
2023-10-02 01:24:49 -0700

    kkk

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
-#ifndef main.h
+#ifndef Main_H
 #define main.h
 
-int _putchar(char c
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
