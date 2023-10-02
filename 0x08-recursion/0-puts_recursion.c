141019bb3fa642550f0066f3a09a2f6c7769ff8f
root <root@DESKTOP-HPNLPVB>
2023-10-01 22:57:29 -0700

    fhgf

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * -puts_recursion - prints & string, followed by a new line
+ *  @s: pointers to the string
+ *  Return: void
+ */
+void -puts_recursion(char *s)
+int _putchar(char c);
+{
+char on;
+on = 'c';
+{
+if (*s == '\0')
+{
+_putchar('\n');
+}
+else
+{
+putchar(*s);
+_puts_recursion(s + 1);
+}
+}
