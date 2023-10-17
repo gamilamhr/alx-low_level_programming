12adebcc732c806d90a6cb00b02ca10efaf81371
gamilamhr <gamilans@icloud.com>
2023-10-17 13:49:53 -0700

    cd.1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,12 +12,12 @@ int main(void)
 	srand (time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if(" n > 0 ")
+	if(n > 0 )
 	
 	{
 	printf("%d\n is positive", n);
 	}
-        else if(" n == 0 ")
+        else if( n == 0 )
 	{
 	printf("%d\n is zero", n);
 	}
