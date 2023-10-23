724140861292dc8b84d39e5cfa1dbf646abe8b4a
gamilamhr <gamilans@icloud.com>
2023-10-22 21:03:30 -0700

    ffff

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _print_rev_recursion-Write a function that prints a string in reverse.
  * @s: the string to print
@@ -7,20 +8,19 @@
 **/
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		return;
-	}
-s++;
-_print_rev_recursion(s + 1);
-s--;
-_putchar(*s);
+    if (*s == '\0')
+    {
+        return;
+    }
+    _print_rev_recursion(s + 1);
+    putchar(*s);
+}
+
 int main(void)
 {
-char str[] = "Colton Walker";
-_print_rev_recursion(str);
+    char str[] = "Colton Walker";
+    _print_rev_recursion(str);
+    putchar('\n');
 
-putchar('\n');
-
-return (0);
+    return 0;
 }
